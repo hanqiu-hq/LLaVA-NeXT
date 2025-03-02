11d1e6b9daca953e04fe583f6aea208ffdaa674e
qiuhan <qiu_han@foxmail.com>
2025-03-03 01:35:09 +0800

    fix bug

--- llava/train/train_dpo.py
+++ llava/train/train_dpo.py
@@ -252,8 +252,11 @@ def find_all_linear_names(model):
         if any(mm_keyword in name for mm_keyword in multimodal_keywords):
             continue
         if isinstance(module, cls):
-            names = name.split(".")
-            lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+            # names = name.split(".")
+            # lora_module_names.add(names[0] if len(names) == 1 else names[-1])
+            if name.endswith("lm_head"):
+                continue
+            lora_module_names.add(name)
 
     if "lm_head" in lora_module_names:  # needed for 16-bit
         lora_module_names.remove("lm_head")
