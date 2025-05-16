8d8f798fa92707a64a7220745f0cd8e5eb7664ce
qiuhan <qiu_han@foxmail.com>
2025-05-16 18:17:53 +0800

    update script for regionrlhf

--- scripts/train/dpo_ov7b_regionrlhf.sh
+++ scripts/train/dpo_ov7b_regionrlhf.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+export OMP_NUM_THREADS=8
+#export NCCL_IB_DISABLE=0
+#export NCCL_IB_GID_INDEX=3
+# export NCCL_IB_HCA=${ARNOLD_RDMA_DEVICE}
+#export NCCL_SOCKET_IFNAME=eth0
+#export NCCL_DEBUG=INFO
+
 VISION_MODEL_VERSION="google/siglip-so400m-patch14-384"
 
 # DPO Stage
@@ -8,27 +15,24 @@ SFT_MODEL="lmms-lab/llava-onevision-qwen2-7b-ov"
 EPOCH=1
 beta=0.1
 
-DATA_PATH=$1
-OUTPUT_DIR=$2
+NUM_GPUS=$1
+DATA_PATH=$2
+OUTPUT_DIR=$3
+LEARNING_RATE=$4
 
-MASTER_ADDR=`scontrol show hostname $SLURM_JOB_NODELIST | head -n1`
 MASTER_PORT=$((RANDOM % 101 + 20001))
-echo $MASTER_ADDR
-echo $MASTER_PORT
-export MASTER_ADDR=$MASTER_ADDR
-export MASTER_PORT=$MASTER_PORT
 
-torchrun --nproc_per_node=1 --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT \
+torchrun --nproc_per_node=$NUM_GPUS --master_port=$MASTER_PORT \
     llava/train/train_dpo.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 \
-    --deepspeed scripts/zero3_offload.json \
+    --deepspeed scripts/zero2.json \
     --model_name_or_path=${SFT_MODEL} \
     --dpo_alpha=1.0 \
     --beta=${beta} \
     --gamma=0 \
     --version $PROMPT_VERSION \
     --data_path=$DATA_PATH \
-    --image_folder $3 \
+    --image_folder "all_data" \
     --unfreeze_mm_vision_tower True \
     --vision_tower ${VISION_MODEL_VERSION} \
     --mm_projector_type mlp2x_gelu \
@@ -49,7 +53,7 @@ torchrun --nproc_per_node=1 --master_addr=$MASTER_ADDR --master_port=$MASTER_POR
     --evaluation_strategy "no" \
     --save_strategy "no" \
     --save_total_limit 1 \
-    --learning_rate $4 \
+    --learning_rate $LEARNING_RATE \
     --weight_decay 0. \
     --warmup_ratio 0.1 \
     --lr_scheduler_type "cosine" \
@@ -60,6 +64,8 @@ torchrun --nproc_per_node=1 --master_addr=$MASTER_ADDR --master_port=$MASTER_POR
     --dataloader_num_workers 4 \
     --lazy_preprocess True \
     --report_to wandb \
-    --dataloader_drop_last True
-
-
+    --dataloader_drop_last True \
+    --image_max_pixels 1690000 \
+    --image_min_pixels 147456 \
+    --shuffle_data True \
+    ${@:5}
